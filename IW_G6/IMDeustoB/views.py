c1af7bda6e2c0d23e44e55b08f2e29e7c4bfd2c2
Ibai Velada <ibai.velada@opendeusto.es>
2025-11-20 09:25:40 +0100

    Formulario ok, pero a medias, pero habra que cambiarlo para la API

--- IW_G6/IMDeustoB/views.py
+++ IW_G6/IMDeustoB/views.py
@@ -8,8 +8,19 @@ from django.views.generic.list import ListView
 # Create your views here.
 
 def lista_peliculas(request):
-    peliculas= Pelicula.objects.all() 
-    return render(request, 'index.html', {'peliculas': peliculas})
+    peliculas= Pelicula.objects.all()
+    generos = Genero.objects.all()
+
+    idsGeneros = request.GET.getlist('skills')
+    skills_ids = [int(skill) for skill in idsGeneros]
+
+    anyoMenor = Pelicula.objects.order_by('anyo').first()
+    anyoMayor = Pelicula.objects.order_by('anyo').last()
+
+    if skills_ids:
+        peliculas = Pelicula.objects.filter(generos__id__in=skills_ids)
+
+    return render(request, 'index.html', {'peliculas': peliculas, 'generos': generos, 'anyoMenor': anyoMenor.anyo, 'anyoMayor': anyoMayor.anyo})
 
 class PeliculaDetailView(DetailView):
     model = Pelicula
