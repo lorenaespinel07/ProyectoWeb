cc176057b5c2f229bc35571c9dcc626a66a50bdd
marta.meso@opendeusto.es <marta.meso@opendeusto.es>
2025-11-26 17:14:19 +0100

    Implementado detalle actor

--- IW_G6/IMDeustoB/views.py
+++ IW_G6/IMDeustoB/views.py
@@ -26,7 +26,7 @@ class PeliculaDetailView(DetailView):
     context_object_name = 'pelicula'
     template_name = 'moviesingle.html'
 
-     def get_queryset(self):
+    def get_queryset(self):
         peli_id = self.kwargs['pk'] 
 
         response = requests.get(f'https://api.themoviedb.org/3/movie/{peli_id}?api_key=aa02e7c79cbe08314c538b9176a77f88&language=es-ES')
@@ -96,7 +96,30 @@ class ActorDetailView(DetailView):
     context_object_name = 'actor'
     template_name = 'celebritysingle.html' 
 
+    # nombre, profesion (actor, productor, directo...), 
+    # año de nacimiento, pais, altura
+    def get_object(self):
+        actor_id = self.kwargs["pk"]
 
+        # Información de detalle del actor
+        detalle = requests.get(
+            f"https://api.themoviedb.org/3/person/{actor_id}?api_key=aa02e7c79cbe08314c538b9176a77f88&language=es-ES"
+        ).json()
+
+        return detalle
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        actor_id = self.kwargs.get('pk') 
+
+        # lista de pelis donde ha salido
+        filmografia = requests.get(
+            f"https://api.themoviedb.org/3/person/{actor_id}/movie_credits?api_key=aa02e7c79cbe08314c538b9176a77f88&language=es-ES"
+        ).json()
+
+        context['filmografia'] = filmografia
+
+        return context
 '''
 
 class GeneroListView(ListView)
